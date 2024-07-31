2c03ec862a202fe7c5559100752ba7b1764235f1
Sascha Nowak <sascha.nowak@netlogix.de>
2024-07-31 10:06:22 +0200

    feature: add Postgre SQL support

--- src/Upsert.php
+++ src/Upsert.php
@@ -1,38 +1,34 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Netlogix\Doctrine\Upsert;
 
+use BackedEnum;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\ParameterType;
-use Netlogix\Doctrine\Upsert\Exception;
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
+use RuntimeException;
+use Stringable;
 
-class Upsert
+final class Upsert
 {
+    private ?string $table = null;
 
-    /**
-     * @var Connection
-     */
-    private $connection;
+    private array $identifiers = [];
 
-    /**
-     * @var string
-     */
-    private $table;
+    private array $fields = [];
 
-    /**
-     * @var array
-     */
-    private $identifiers = [];
+    private function __construct(
+        private readonly Connection $connection
+    ) {
+    }
 
-    /**
-     * @var array
-     */
-    private $fields = [];
-
-    private function __construct(Connection $connection)
+    public static function fromConnection(Connection $connection): self
     {
-        $this->connection = $connection;
+        return new self($connection);
     }
 
     public function forTable(string $table): self
@@ -42,14 +38,15 @@ class Upsert
         return $this;
     }
 
-    public function withIdentifier(string $column, $value, int $parameterType = ParameterType::STRING): self
+    public function withIdentifier(string $column, mixed $value, int $parameterType = ParameterType::STRING): self
     {
-        if (array_key_exists($column, $this->identifiers)) {
-            throw new Exception\IdentifierAlreadyInUse(sprintf('The identifier "%s" has already been set!', $column), 1603196381);
+        $this->throwErrorIsColumnExists($column);
+
+        if (is_object($value) && method_exists($value, 'rawType')) {
+            $parameterType = $value->rawType();
         }
-        if (array_key_exists($column, $this->fields)) {
-            throw new Exception\IdentifierRegisteredAsField(sprintf('The identifier "%s" has already been set as field!', $column), 1603197666);
-        }
+
+        $value = $this->getValue($value);
 
         $this->identifiers[$column] = [
             'value' => $value,
@@ -59,18 +56,24 @@ class Upsert
         return $this;
     }
 
-    public function withField(string $column, $value, int $parameterType = ParameterType::STRING): self
-    {
-        if (array_key_exists($column, $this->fields)) {
-            throw new Exception\FieldAlreadyInUse(sprintf('The field "%s" has already been set!', $column), 1603196457);
+    public function withField(
+        string $column,
+        mixed $value,
+        int $parameterType = ParameterType::STRING,
+        bool $insertOnly = false
+    ): self {
+        $this->throwErrorIsColumnExists($column);
+
+        if (is_object($value) && method_exists($value, 'rawType')) {
+            $parameterType = $value->rawType();
         }
-        if (array_key_exists($column, $this->identifiers)) {
-            throw new Exception\FieldRegisteredAsIdentifier(sprintf('The field "%s" has already been set as identifier!', $column), 1603197691);
-        }
+
+        $value = $this->getValue($value);
 
         $this->fields[$column] = [
             'value' => $value,
             'type' => $parameterType,
+            'insertOnly' => $insertOnly,
         ];
 
         return $this;
@@ -78,25 +81,30 @@ class Upsert
 
     public function execute(): int
     {
-        if (!$this->table) {
+        if ($this->table === null) {
             throw new Exception\NoTableGiven('No table name has been set!', 1603199471);
         }
-        if (count($this->identifiers) === 0 || count($this->fields) === 0) {
+
+        if ($this->identifiers === [] || $this->fields === []) {
             throw new Exception\EmptyUpsert('No columns have been specified for upsert!', 1603199389);
         }
+
+        $identifiers = implode(', ', array_keys($this->identifiers));
 
         $allFields = array_merge($this->fields, $this->identifiers);
 
         $columns = implode(', ', array_keys($allFields));
-        $values = implode(', ', array_map(function(string $column) {
-            return ':' . $column;
-        }, array_keys($allFields)));
+        $values = implode(', ', array_map(static fn (string $column): string => ':' . $column, array_keys($allFields)));
 
-        $updates = implode(', ', array_map(function(string $column) {
-            return $column . ' = :' . $column;
-        }, array_keys($this->fields)));
+        $updates = implode(
+            ', ',
+            array_map(
+                static fn (string $column): string => $column . ' = :' . $column,
+                array_keys(array_filter($this->fields, static fn (array $field): bool => !$field['insertOnly']))
+            )
+        );
 
-        $sql = $this->buildQuery($columns, $values, $updates);
+        $sql = $this->buildQuery($identifiers, $columns, $values, $updates);
 
         $result = $this->connection->executeQuery(
             $sql,
@@ -107,33 +115,66 @@ class Upsert
         return $result->rowCount();
     }
 
-    protected function buildQuery(string $columns, string $values, string $updates): string
+    protected function buildQuery(string $identifiers, string $columns, string $values, string $updates): string
     {
-        switch($this->connection->getDatabasePlatform()->getName()) {
-            case 'mysql':
-                return <<<MYSQL
+        $platform = $this->connection->getDatabasePlatform();
+
+        return match (true) {
+            $platform instanceof PostgreSQLPlatform => <<<POSTGRESQL
+INSERT INTO "{$this->table}" ({$columns})
+VALUES ({$values})
+ON CONFLICT ({$identifiers}) DO UPDATE SET {$updates}
+POSTGRESQL
+        ,
+            $platform instanceof AbstractMySQLPlatform => <<<MYSQL
 INSERT INTO {$this->table} ({$columns})
 VALUES ({$values})
 ON DUPLICATE KEY UPDATE {$updates}
-MYSQL;
-            case 'sqlite':
-                $conflictColumns = implode(', ', array_keys($this->identifiers));
-                return <<<SQLITE
+MYSQL
+        ,$platform instanceof SqlitePlatform => <<<SQLITE
 INSERT INTO {$this->table} ({$columns})
 VALUES ({$values})
-ON CONFLICT({$conflictColumns}) DO UPDATE SET {$updates}
-SQLITE;
-            default:
-                throw new Exception\UnsupportedDatabasePlatform(
-                    sprintf('The database platform %s is not supported!', $this->connection->getDatabasePlatform()->getName()),
-                    1603199935
-                );
+ON CONFLICT({$identifiers}) DO UPDATE SET {$updates}
+SQLITE
+            ,default => throw new RuntimeException(
+                sprintf('The database platform %s is not supported!', $platform::class),
+                1_603_199_935
+            )
+        };
+    }
+
+    private function getValue(mixed $value): int|string|float|bool|null
+    {
+        if (is_array($value)) {
+            $value = json_encode($value, JSON_THROW_ON_ERROR);
+        }
+
+        if ($value instanceof Stringable) {
+            $value = (string) $value;
+        }
+
+        if ($value instanceof BackedEnum) {
+            $value = (string) $value->value;
+        }
+
+        if (is_object($value) && method_exists($value, 'rawValue')) {
+            return $value->rawValue();
+        }
+
+        return $value;
+    }
+
+    private function throwErrorIsColumnExists(string $column): void
+    {
+        if (array_key_exists($column, $this->fields)) {
+            throw new Exception\FieldAlreadyInUse(sprintf('The field "%s" has already been set!', $column), 1603196457);
+        }
+
+        if (array_key_exists($column, $this->identifiers)) {
+            throw new Exception\IdentifierRegisteredAsField(
+                sprintf('The field "%s" has already been set as identifier!', $column),
+                1603197691
+            );
         }
     }
-
-    public static function fromConnection(Connection $connection): self
-    {
-        return new static($connection);
-    }
-
 }
